1a7e52341c38a26cff3b74afc676ffc16243e13e
Matthy <csaki.mateki@gmail.com>
2023-12-16 14:25:06 +0100

    PLAYER - PPS

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 200
 var lookspeed = 10
-var drag = 0.92
+var drag = 0.8
 var direction = Vector2(0, 0)
+
+var dash_cooldown = 0
+var D_dash_coldown = 40
+var dash_amount = 3
 
 func _process(delta):
 	move_and_slide()
@@ -14,6 +18,8 @@ func _process(delta):
 
 func _physics_process(delta):
 	direction = Vector2(0, 0)
+	velocity = velocity * drag
+	
 	if Input.is_action_pressed("ui_left"):
 		direction.x = -1
 		velocity.x -= 1 * speed
@@ -26,5 +32,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 		velocity.y -= 1 * speed
+	if Input.is_action_just_pressed("space"):
+		dash()
+		
+	if dash_amount < 3:
+		if dash_cooldown > 0:
+			dash_cooldown -= 1
+		if dash_cooldown == 0:
+			dash_cooldown = D_dash_coldown
+			dash_amount += 1
+		
+func dash():
+	if dash_amount > 0:
+		velocity = velocity * 4
+		dash_amount -= 1
 
-	velocity = velocity * drag
